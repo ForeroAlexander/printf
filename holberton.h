3f9dac8527f7e1ad4e6ea7701811c9898cad7b93
ForeroAlexander <foreroalex26@gmail.com>
2021-03-14 22:16:19 +0000

    header file

--- holberton.h
+++ holberton.h
@@ -5,19 +5,25 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+/**
+ *struct all - strcuture function
+ *@all: pointer to a char
+ *@f: poniter to a function
+ */
 typedef struct all
 {
 	char *all;
 	int (*f)();
-}all_t;
+} all_t;
+
+int (*get_func(char s))(va_list);
 int _printf(const char *format, ...);
 int print_char(va_list arguments_list);
 int print_string(va_list arguments_list);
 int _putchar(char c);
 int print_number(va_list arguments_list);
 int print_rev(va_list arguments_list);
-int print_int(va_list arguments_list);
+int print_integer(va_list arguments_list);
 int print_i(int n);
 
 #endif
