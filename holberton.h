031c5c8d507271ae46e27e2739f7d2e4a2ecc3b0
ForeroAlexander <foreroalex26@gmail.com>
2021-03-14 21:35:09 +0000

    Header

--- holberton.h
+++ holberton.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef struct all
 {
@@ -11,12 +12,12 @@ typedef struct all
 	int (*f)();
 }all_t;
 int _printf(const char *format, ...);
-int print_char(va_list list);
-int print_string(va_list list);
+int print_char(va_list arguments_list);
+int print_string(va_list arguments_list);
 int _putchar(char c);
-int print_number(va_list list);
+int print_number(va_list arguments_list);
+int print_rev(va_list arguments_list);
+int print_int(va_list arguments_list);
 int print_i(int n);
-int print_rev(va_list list);
-int print_int(va_list list);
 
 #endif
