fc49ddb3c0fd49e66f21b46167f9ecb880664486
Carolinacapote <2621@holbertonschool.com>
2021-03-14 22:15:27 +0000

    Finding the function to be used

---
+++ get_function.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * get_func - Function to find the function to be used.
+ * @s: Char to find an specific letter or symbol.
+ * Return: Always 0.
+ */
+int (*get_func(char s))(va_list)
+{
+	all_t all[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_number},
+		{"i", print_integer},
+		{"r", print_rev},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (all[i].all != '\0')
+	{
+		if (s == *all[i].all)
+			return (all[i].f);
+		i++;
+	}
+	return ('\0');
+}
